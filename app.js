84c6ac18adcfbdf774914e63cbf7766793ff7949
Rahul Sabinkar <rahulsabinkar4u@gmail.com>
2022-12-15 21:00:44 +0530

    add multiple lists
    - Modular views
    - Add multiple lists functionality

--- app.js
+++ app.js
@@ -5,23 +5,34 @@ const bodyParser = require('body-parser');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 
 let items = ['Get milk', 'Get bread', 'Get eggs'];
+let workItems = [];
 
 app.get('/', (req, res) => {
   let date = new Date();
-  const options = { weekday: 'long', month: 'long', day: 'numeric'};
+  const options = { weekday: 'long', month: 'short', day: 'numeric'};
   let today = date.toLocaleDateString('en-US', options);
 
-  res.render('list', {day: today, newItemsList: items});
+  res.render('list', {listTitle: today, newItemsList: items});
 })
 
 app.post('/', (req, res) => {
   let item = req.body.newItemInput;
-  items.push(item);
-  res.redirect('/');
+  if (req.body.list === 'Work') {
+    workItems.push(item);
+    res.redirect('/work');
+  } else {
+    items.push(item);
+    res.redirect('/');
+  }
+})
+
+app.get('/work', (req, res) => {
+  res.render('list', {listTitle: "Work", newItemsList: workItems});
 })
 
 app.listen(PORT, ()=> {
